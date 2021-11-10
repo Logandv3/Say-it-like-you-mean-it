40c4bcf4c48a5ec48e388e9d8a621e8a78d9ebbb
Logan Vincent <ldvsimp@gmail.com>
2021-11-09 19:06:04 -0700

    Add component files and change app to class component

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import './styles/App.css'
+import Feedback from './Feedback'
+import AllEntries from './AllEntries'
+import FlaggedEntries from './FlaggedEntries'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className="app-container">
+        <header className="app-header">
+          
+        </header>
+      </div>
+    )
+  }
+}
+
+export default App
