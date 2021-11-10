0f7815d9f333ad53270b85306e59b678d4f79b5f
Logan Vincent <ldvsimp@gmail.com>
2021-11-09 19:38:11 -0700

    Create Card component

---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Card.css'
+
+
+const Card = () => {
+
+  return(
+    <section className='entry-card'>
+      <p>This is where info about a specific entry will go</p>
+    </section>
+  )
+}
+
+export default Card
