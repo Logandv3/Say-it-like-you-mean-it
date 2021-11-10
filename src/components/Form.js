30dad75d08390df247fe8f36adf036a5d6279ea6
Logan Vincent <ldvsimp@gmail.com>
2021-11-10 14:24:37 -0700

    Add variable to disable button when fields are empty

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,12 +5,13 @@ import { useHistory } from 'react-router-dom'
 
 const Form = ({ formInfo, onChange, currentEntry }) => {
   let history = useHistory()
+  let btnStatus = (formInfo.title && formInfo.content) ? false : true
   
   return(
     <form className='entry-form'>
       <input type='text' placeholder='Enter title of content' name='title' onChange={(event) => onChange(event.target)} value={formInfo.title} />
       <input type='text' placeholder='Enter content you would like to analyze' name='content' onChange={(event) => onChange(event.target)} value={formInfo.content} />
-      <button type='button' className='analyze-entry-btn' onClick={() => history.push(`/${currentEntry.id}`)} >Analyze</button>
+      <button type='button' disabled={btnStatus} className='analyze-entry-btn' onClick={() => history.push(`/${currentEntry.id}`)} >Analyze</button>
     </form>
   )
 }
