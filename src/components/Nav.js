28373ea6ef2c17f2e9b1fd7b86c6c916b302a8ff
Logan Vincent <ldvsimp@gmail.com>
2021-11-09 19:19:56 -0700

    Add basic info to Nav and Form

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,3 +1,16 @@
 import React from 'react'
+import './styles/Nav.css'
 import AllEntries from './AllEntries'
 import FlaggedEntries from './FlaggedEntries'
+
+const Nav = () => {
+
+  return(
+    <section>
+      <AllEntries />
+      <FlaggedEntries />
+    </section>
+  )
+}
+
+export default Nav
