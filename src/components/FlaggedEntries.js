7e778f0c7bad23f7d23ec1ee09e102b0e146aae4
Logan Vincent <ldvsimp@gmail.com>
2021-11-09 19:36:12 -0700

    Create FlaggedEntries component basics and Card files

---
+++ src/components/FlaggedEntries.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/FlaggedEntries.css'
+
+
+const FlaggedEntries = () => {
+
+  return(
+    <section className='flagged-entries-container'>
+      <p>This is where all user entry cards will go</p>
+    </section>
+  )
+}
+
+export default FlaggedEntries
