ed96a97c46248bf2e79f708f1d0b2dc30fded062
Logan Vincent <ldvsimp@gmail.com>
2021-11-09 19:34:21 -0700

    Create AllEntries component basics

---
+++ src/components/AllEntries.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/AllEntries.css'
+
+
+const AllEntries = () => {
+
+  return(
+    <section className='all-entries-container'>
+      <p>This is where all user entry cards will go</p>
+    </section>
+  )
+}
+
+export default AllEntries
