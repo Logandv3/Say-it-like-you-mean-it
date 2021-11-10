6bd38bb6a988db17d4917a1f90adaf2e1141e32d
Logan Vincent <ldvsimp@gmail.com>
2021-11-09 19:32:55 -0700

    Create Feedback component basics

---
+++ src/components/Feedback.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Feedback.css'
+
+
+const Feedback = () => {
+
+  return(
+    <section className='feedback-container'>
+      <p>This is where the feedback will go</p>
+    </section>
+  )
+}
+
+export default Feedback
