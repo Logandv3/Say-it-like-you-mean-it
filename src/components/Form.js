abfb9a2d43c6e5ab37da47ba5e3fc3cbb06fffd8
Logan Vincent <ldvsimp@gmail.com>
2021-11-10 20:40:21 -0700

    Add fetch POST and store response in current entry

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import '../styles/Form.css'
 import { useHistory } from 'react-router-dom'
+import { submitEntry } from './apiCalls'
 
 
-const Form = ({ formInfo, onChange, currentEntry }) => {
-  let history = useHistory()
+const Form = ({ formInfo, onChange, currentEntry, cleanResponse }) => {
   let btnStatus = (formInfo.title && formInfo.content) ? false : true
+  let history = useHistory()
 
   return(
     <form className='entry-form'>
       <input type='text' placeholder='Enter title of content' name='title' onChange={(event) => onChange(event.target)} value={formInfo.title} />
       <input type='text' placeholder='Enter content you would like to analyze' name='content' onChange={(event) => onChange(event.target)} value={formInfo.content} />
-      <button type='button' disabled={btnStatus} className='analyze-entry-btn' onClick={() => history.push(`/${currentEntry.id}`)} >Analyze</button>
+      <button type='button' disabled={btnStatus} className='analyze-entry-btn' onClick={() => submitEntry(currentEntry.content, cleanResponse) && history.push(`/${currentEntry.id}`)}>Analyze</button>
     </form>
   )
 }
