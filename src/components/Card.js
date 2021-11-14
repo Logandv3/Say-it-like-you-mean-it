0db9f285bf1f6b126ae0d47d33da85e03e7dbb19
Logan Vincent <ldvsimp@gmail.com>
2021-11-13 17:03:30 -0700

    Add basic styling to past entries and card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,12 +3,14 @@ import { useHistory } from 'react-router-dom'
 import '../styles/Card.css'
 
 
-const Card = ({ title, id, addEntry }) => {
+const Card = ({ title, emotion, perception, id, addEntry }) => {
   let history = useHistory()
 
   return(
     <section className='entry-card' key={id} onClick={() => addEntry(id) && history.push(`/${id}`)} >
       <p>{title}</p>
+      <p>{emotion}</p>
+      <p>{perception}</p>
     </section>
   )
 }
--- src/components/PastEntryView.js
+++ src/components/PastEntryView.js
@@ -7,18 +7,22 @@ import Card from './Card'
 const PastEntryView = ({ viewType, entries, toggleEntry }) => {
 
   let displayEntries = viewType === 'all' ? 
-  entries.map(entry => <Card title={entry.title} key={entry.id} id={entry.id} addEntry={toggleEntry} />) : 
+  entries.map(entry => <Card title={entry.title} emotion={entry.entryAnalysis.primaryEmotion} perception={entry.entryAnalysis.perceivedAs} key={entry.id} id={entry.id} addEntry={toggleEntry} />) : 
   entries.filter((entry) => {
-    return !entry.flagged ? false : <Card title={entry.title} key={entry.id} id={entry.id} />
+    return !entry.flagged ? false : <Card title={entry.title} emotion={entry.entryAnalysis.primaryEmotion} perception={entry.entryAnalysis.perceivedAs} key={entry.id} id={entry.id} />
   })
+
+  const viewTitle = viewType === 'all' ? 'All Entries' : 'Flagged Entries'
 
   return(
     <section className='past-entries-view'>
       <Link to='/'>
         <h3 className='back-to-main-link'>--Back to Main Page</h3>
       </Link>
-      <p>This is where all user entry cards will go</p>
-      <div className='entries-container'>{displayEntries}</div>
+      <section className='entries-container'>
+        <h2>{viewTitle}</h2>
+        <div className='card-container'>{displayEntries}</div>
+      </section>
     </section>
   )
 }
